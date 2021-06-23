b7c3ec2642da7cafcf737e3888a0c74020884b32
mrityun-d <63406642+mrityun-d@users.noreply.github.com>
2021-06-23 20:54:55 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+    public static void main( String []args ) {
+        System.out.println( "Hello World!" );
+        System.out.println( "Deploy to Tomcat apache server");
+    }
+}
